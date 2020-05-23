211ceadd612c09f4dcc77c5a70d00a9147f56e06
David Almeida <davidalmeidadrive@gmail.com>
2020-05-23 10:45:13 -0300

    complete

---
+++ RoomRental/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RoomRental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Rental[] vect = new Rental[10];
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Rental #{i}: ");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                vect[room] = new Rental(name, email);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Busy rooms: ");
+            for(int i=0; i < vect.Length; i++)
+            {
+                if(vect[i] != null)
+                {
+                    Console.WriteLine($"{i}: {vect[i].Name}, {vect[i].Email}");
+                }
+            }
+        }
+    }
+}
---
+++ RoomRental/Rental.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoomRental
+{
+    class Rental
+    {
+        public string Name { get; private set; }
+        public string  Email { get; private set; }
+
+        public Rental(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
+    }
+}
